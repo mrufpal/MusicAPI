a9ebecb1a883650978f358027f002120df6d6146
José Antonio Parejo Maestre <japarejo@gmail.com>
2017-11-08 15:00:36 +0100

    Añadido proyecto de base y ficheros de configuración

---
+++ src/main/java/api/resources/SongResource.java
@@ -0,0 +1,84 @@
+package api.resources;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.jboss.resteasy.spi.BadRequestException;
+import org.jboss.resteasy.spi.NotFoundException;
+
+import model.Playlist;
+import model.Song;
+import model.repository.MapPlaylistRepository;
+import model.repository.PlaylistRepository;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+
+
+@Path("/songs")
+public class SongResource {
+
+	public static SongResource _instance=null;
+	PlaylistRepository repository;
+	
+	private SongResource(){
+		repository=MapPlaylistRepository.getInstance();
+	}
+	
+	public static SongResource getInstance()
+	{
+		if(_instance==null)
+			_instance=new SongResource();
+		return _instance; 
+	}
+	
+	@GET
+	@Produces("application/json")
+	public Collection<Song> getAll()
+	{
+		return null;
+	}
+	
+	
+	@GET
+	@Path("/{id}")
+	@Produces("application/json")
+	public Song get(@PathParam("id") String songId)
+	{
+		
+		return null;
+	}
+	
+	@POST
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response addSong(@Context UriInfo uriInfo, Song song) {
+		return null;
+	}
+	
+	
+	@PUT
+	@Consumes("application/json")
+	public Response updateSong(Song song) {
+		return null;
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response removeSong(@PathParam("id") String songId) {
+		return null;
+	}
+	
+}
