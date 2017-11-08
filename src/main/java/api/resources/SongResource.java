e81316643d00339d569444488f245717c88f2809
Macarena Rufo <mrufpal@gmail.com>
2017-11-08 20:32:56 +0100

    Añadidos metodos API song

--- src/main/java/api/resources/SongResource.java
+++ src/main/java/api/resources/SongResource.java
@@ -26,59 +26,100 @@ import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
-
 @Path("/songs")
 public class SongResource {
 
-	public static SongResource _instance=null;
-	PlaylistRepository repository;
-	
-	private SongResource(){
-		repository=MapPlaylistRepository.getInstance();
-	}
-	
-	public static SongResource getInstance()
-	{
-		if(_instance==null)
-			_instance=new SongResource();
-		return _instance; 
-	}
-	
-	@GET
-	@Produces("application/json")
-	public Collection<Song> getAll()
-	{
-		return null;
-	}
-	
-	
-	@GET
-	@Path("/{id}")
-	@Produces("application/json")
-	public Song get(@PathParam("id") String songId)
-	{
-		
-		return null;
-	}
-	
-	@POST
-	@Consumes("application/json")
-	@Produces("application/json")
-	public Response addSong(@Context UriInfo uriInfo, Song song) {
-		return null;
-	}
-	
-	
-	@PUT
-	@Consumes("application/json")
-	public Response updateSong(Song song) {
-		return null;
-	}
-	
-	@DELETE
-	@Path("/{id}")
-	public Response removeSong(@PathParam("id") String songId) {
-		return null;
-	}
-	
+    public static SongResource _instance = null;
+    PlaylistRepository repository;
+
+    private SongResource() {
+        repository = MapPlaylistRepository.getInstance();
+    }
+
+    public static SongResource getInstance() {
+        if (_instance == null) {
+            _instance = new SongResource();
+        }
+        return _instance;
+    }
+
+    @GET
+    @Produces("application/json")
+    public Collection<Song> getAll() {
+        return repository.getAllSongs();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Song get(@PathParam("id") String songId) {
+        Song song = repository.getSong(songId);
+
+        if (song == null) {
+            throw new NotFoundException("The song with id=" + songId + " was not found");
+        }
+        return song;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response addSong(@Context UriInfo uriInfo, Song song) {
+        if (song.getTitle() == null || "".equals(song.getTitle())) {
+            throw new BadRequestException("The title of the song must not be null");
+        }
+
+        repository.addSong(song);
+
+        // Builds the response. Returns the song the has just been added.
+        UriBuilder ub = uriInfo.getAbsolutePathBuilder().path(this.getClass(), "get");
+        URI uri = ub.build(song.getId());
+        ResponseBuilder resp = Response.created(uri);
+        resp.entity(song);
+        return resp.build();
+    }
+
+    @PUT
+    @Consumes("application/json")
+    public Response updateSong(Song song) {
+        Song oldsong = repository.getSong(song.getId());
+        if (oldsong == null) {
+            throw new NotFoundException("The song with id=" + song.getId() + " was not found");
+        }
+
+        // Update tittle
+        if (song.getTitle() != null) {
+            oldsong.setTitle(song.getTitle());
+        }
+
+        // Update album
+        if (song.getAlbum() != null) {
+            oldsong.setAlbum(song.getAlbum());
+        }
+        
+        // Update artist
+        if (song.getArtist() != null) {
+            oldsong.setArtist(song.getArtist());
+        }
+        
+        // Update year
+        if (song.getYear() != null) {
+            oldsong.setYear(song.getYear());
+        }
+
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeSong(@PathParam("id") String songId) {
+        Song song = repository.getSong(songId);
+        if (song == null) {
+            throw new NotFoundException("The song with id=" + song.getId() + " was not found");
+        }
+        repository.deleteSong(songId);
+        
+        return Response.noContent().build();
+    }
+
 }
